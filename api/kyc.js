8dc064a1b3b8848e38fd8cda29fab91dccaca562
leonlee-xinwealth <leon.lee@xinwealth.com>
2026-04-25 11:47:18 +0800

    fix: remaining ESM imports extensions

--- api/kyc.js
+++ api/kyc.js
@@ -1,4 +1,4 @@
-import { supabaseAdmin } from './_lib/supabase';
+import { supabaseAdmin } from './_lib/supabase.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
